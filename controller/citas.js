8c1d7f118c332c25cf45233a2a8e0fc1daa5ff35
Luis Angel Bustamante <luisbustamante1571@gmail.com>
2023-03-22 11:29:18 -0500

    Registro medico funcional: Eliminar y Actualizar registros

--- controller/citas.js
+++ controller/citas.js
@@ -7,7 +7,7 @@ function obtenerCitas(event) {
     .then(response => response.json())
     .then(data => {
       // Aquí puedes manipular los datos obtenidos y actualizar la vista
-      // console.log(data);
+      console.log(event);
 
       if (data && data.data && data.data.length > 0) {
         // traemos la data le paciente desde el objeto global
@@ -28,16 +28,21 @@ function obtenerCitas(event) {
             if (cita.Paciente_Identificacion == paciente.Identificacion) {
               nombre_paciente = `${paciente.Nombre} ${paciente.Apellido}`
             }
-            // console.log(paciente.Nombre)
+            // console.log(paciente.Identificacion)
           });
 
           document.getElementById("form").reset();
 
-          const atendida = cita.Atendida === 1 ? "Atendida" : "Pendiente";
+          console.log(cita.Atendida);
+          let atendida = cita.Atendida === 0 ? "Atendida" : "Pendiente";
+          
           // console.log(cita.id);
           // console.log(cita.Paciente_Identificacion);
+          console.log(atendida);
+
+
           tablaHTML += `
-            <tr class="botones-fila" data-id"${ cita.id }">
+            <tr class="botones-fila" data-id="${cita.id}">
               <td id="fecha">${cita.Fecha}</td>
               <td id="hora">${cita.Hora}</td>
               <td id="paciente">${cita.Paciente_Identificacion}</td>
@@ -45,13 +50,14 @@ function obtenerCitas(event) {
               <td id="valor">${cita.Valor}</td>
               <td id="atendida">${atendida}</td>
               <td class="botones-celda">
-                <input class="boton-eliminar" onclick="eliminarCita(event, ${cita.id})" value="Eliminar" type="submit">
-                <input class="boton-actualizar" onclick="actualizarCitas(event, ${cita.id})" value="Actualizar" type="submit">
+                <button class="boton-eliminar" onclick="eliminarCita(event, ${cita.id})" >Eliminar</button>
+                <button class="boton-actualizar" onclick="actualizarCitas(event, ${cita.id}, ${cita.Atendida})" >Actualizar Estado</button>
+                
               </td>
             </tr>
           `;
         });
-        
+
 
         // Asignamos el HTML acumulado al cuerpo de la tabla
         tbody.innerHTML = tablaHTML;
@@ -104,7 +110,7 @@ function agregarCita(event) {
       // Aquí puedes manipular los datos obtenidos y actualizar la vista
       console.log(data);
       document.getElementById("formulario-cita").reset();
-      
+
       const mensaje = document.getElementById('mensaje');
       mensaje.textContent = data.message; // Agregar el mensaje devuelto por la petición
       mensaje.classList.add('mostrar');
@@ -119,73 +125,59 @@ function agregarCita(event) {
 }
 
 // Función para actualizar una cita existente en la base de datos
-function actualizarCitas( event, id ) {
+function actualizarCitas(event, id, atendida) {
+
+
+  // console.log(id, fecha, hora, paciente_id, atendida);
 
   event.preventDefault();
 
-  
-
+  if (atendida === 0) {
+    atendida = 1
+  } else {
+    atendida = 0
+  }
 
 
   // Realizar la petición de actualización
-  fetch(`http://localhost/citas-electiva/Model/citas_model.php?id=${ id }`, {
+  fetch(`http://localhost/citas-electiva/Model/citas_model.php?id=${id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
       id: id,
-      fecha: fecha,
-      hora: hora,
-      paciente_id: paciente_id,
-      medico_identificacion: medico_identificacion,
-      atendida: atendida,
+      atendida: atendida
     })
   })
-  .then(response => response.json())
-  .then(data => {
-    // Aquí puedes manipular los datos obtenidos y actualizar la vista
-    console.log(data);
+    .then(response => response.json())
+    .then(data => {
+      // Aquí puedes manipular los datos obtenidos y actualizar la vista
+      console.log(data);
+      // Actualizar el valor de atendida en la tabla HTML
+      // obtenerCitas(event); // Llamamos a la función obtenerCitas para actualizar la vista
+      const fila = document.querySelector(`tr[data-id="${id}"]`);
+      const atendidaHTML = fila.querySelector('#atendida');
+      atendidaHTML.textContent = atendida === 0 ? 'Pendiente' : 'Atendida';
+      const mensaje = document.getElementById("mensaje");
+      if (mensaje) {
+        mensaje.textContent = data.message;
+        mensaje.classList.add('mostrar');
+        setTimeout(() => {
+          mensaje.classList.remove('mostrar');
+        }, 5000);
+      }
 
-    const mensaje = document.getElementById('mensaje');
-    mensaje.textContent = data.mensaje; // Agregar el mensaje devuelto por la petición
-    mensaje.classList.add('mostrar');
-
-    setTimeout(() => {
-      mensaje.classList.remove('mostrar'); // Remover la clase 'mostrar' para que se oculte
-    }, 5000);
-
-
-    // Si se actualizó correctamente, actualizar la fila correspondiente en la tabla
-    const filaActualizar = document.querySelector(`tr[data-id="${id}"]`);
-
-    // Si existe la fila, actualizar el contenido de las celdas correspondientes a cada campo
-    if (filaActualizar) {
-      const celdaFecha = filaActualizar.querySelector('#fecha');
-      celdaFecha.textContent = fecha;
-
-      const celdaHora = filaActualizar.querySelector('#hora');
-      celdaHora.textContent = hora;
-
-      const celdaPaciente = filaActualizar.querySelector('#paciente');
-      celdaPaciente.textContent = paciente_id;
-
-      const celdaMedico = filaActualizar.querySelector('#medico');
-      celdaMedico.textContent = medico_identificacion;
-
-      // const celdaAtendida = filaActualizar.querySelector('#atendida');
-      // celdaAtendida.textContent = atendida;
-    }
-  })
-  .catch(error => {
-    console.error('Error al actualizar cita:', error);
-  });
+    })
+    .catch(error => {
+      console.error('Error al actualizar cita:', error);
+    });
 }
 
 // Función para eliminar una cita de la base de datos
-function eliminarCita( event, id) {
+function eliminarCita(event, id) {
   event.preventDefault();
-  fetch(`http://localhost/citas-electiva/Model/citas_model.php?id=${ id }`, {
+  fetch(`http://localhost/citas-electiva/Model/citas_model.php?id=${id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json'
@@ -196,22 +188,14 @@ function eliminarCita( event, id) {
   })
     .then(response => response.json())
     .then(data => {
-      // Aquí puedes manipular los datos obtenidos y actualizar la vista
       console.log(data);
-      // Buscar la fila correspondiente al registro eliminado
-      const filaEliminar = document.querySelector(`tr[data-id="${id}"]`);
-      // Si existe la fila, eliminarla de la tabla
-      if (filaEliminar) {
-        filaEliminar.remove();
-      }
+      obtenerCitas(event); // Llamamos a la función obtenerCitas para actualizar la vista
       const mensaje = document.getElementById('mensaje');
-      mensaje.textContent = data.mensaje; // Agregar el mensaje devuelto por la petición
+      mensaje.textContent = data.mensaje;
       mensaje.classList.add('mostrar');
-
       setTimeout(() => {
-        mensaje.classList.remove('mostrar'); // Remover la clase 'mostrar' para que se oculte
+        mensaje.classList.remove('mostrar');
       }, 5000);
-
     })
     .catch(error => {
       console.error('Error al eliminar cita:', error);
